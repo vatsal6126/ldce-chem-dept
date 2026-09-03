0721d5795a74552ec2a055fe9a9b130a964d5d19
vatsal <vapatel6126@gmail.com>
2026-09-03 14:10:07 +0530

    everything should work

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: '/ldce-chem-dept/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
 });
