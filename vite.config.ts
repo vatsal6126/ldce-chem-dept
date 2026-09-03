c11a6a2ad7266f6c93acc3b8fd97c578bbee066d
vatsal <vapatel6126@gmail.com>
2026-09-03 14:47:13 +0530

    blur changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  // Tells Vite your site lives inside the repository subfolder
   base: '/ldce-chem-dept/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
 });
